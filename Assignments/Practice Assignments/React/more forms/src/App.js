ec269c7e7302e6c2bf37e3da86b1c72b8f511c0e
oussama0710 <ouxavi80@gmail.com>
2023-03-13 17:13:48 +0100

    oussema

---
+++ Assignments/Practice Assignments/React/more forms/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Registration from './components/registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration/>
+    </div>
+  );
+}
+
+
+export default App;
