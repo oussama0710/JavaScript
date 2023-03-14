aa87ddf279c0eaf3871217f854eb00d9995f0485
oussama0710 <ouxavi80@gmail.com>
2023-03-14 21:14:29 +0100

    oussema

---
+++ Assignments/Practice Assignments/React/pokemon-api/src/components/Fetch.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Fetch = (props) => {
+  const [pokemon, setPokemon] = useState([]);
+  const fetchPok = (e) => {
+    e.preventDefault();
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response.results);
+        setPokemon(response.results);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <button
+        onClick={(e) => fetchPok(e)}>
+        Fetch Pokemon
+      </button>
+      
+      {pokemon.map((element,i) => {return <p key={i}>{element.name}</p>}
+      )}
+    </div>
+  );
+};
+
+export default Fetch;
